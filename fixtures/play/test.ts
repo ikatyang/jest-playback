5af32a57b266f63fec0b2e4d1c5f6edf64a9f734
ikatyang <ikatyang@gmail.com>
2017-07-13 17:58:42 +0800

    test(play): add tests

---
+++ fixtures/play/test.ts
@@ -0,0 +1,29 @@
+import * as request from 'request';
+
+it('should mock correctly for same scope case 1', done => {
+  request('http://www.example.com/?case=1', (_err, _res, body) => {
+    expect(body).toMatchSnapshot();
+    done();
+  });
+});
+
+test('should mock correctly for same scope case 2', done => {
+  request('http://www.example.com/?case=2', (_err, _res, body) => {
+    expect(body).toMatchSnapshot();
+    done();
+  });
+});
+
+test('should mock correctly for other scope', done => {
+  request('http://example.org/', (_err, _res, body) => {
+    expect(body).toMatchSnapshot();
+    done();
+  });
+});
+
+test('should return undefined for unmocked scope', done => {
+  request('http://something.else/', (_err, _res, body) => {
+    expect(body).toBeUndefined();
+    done();
+  });
+});
---
+++ tests/reporter.ts
@@ -0,0 +1,14 @@
+import {exec} from 'child_process';
+import * as path from 'path';
+
+const test_fixture = (test_fn: typeof test, name: string) => {
+  test_fn(name, done => {
+    const dirname = path.resolve(__dirname, '../fixtures', name);
+    exec(`jest -c ${dirname}/jest.json`, err => {
+      expect(err).toBeFalsy();
+      done();
+    });
+  });
+};
+
+test_fixture(test, 'play');
